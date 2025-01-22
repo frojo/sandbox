9afa3ee2181d731e0c06e7f54260df3b59fab2ab
Fran Rojo <franmorojo@gmail.com>
2025-01-21 20:13:57 -0500

    woo

--- .eleventy.js
+++ .eleventy.js
@@ -4,12 +4,22 @@ export default function (config) {
   //   return string.charAt(0).toUpperCase() + string.slice(1)
   // })
 
+
+  // config.addCollection("project", function(api) {
+  //   console.log(api.getFilteredByTag('project'));
+  //   return [];
+  // })
+
+  // config.addTemplateFormats("css, js");
+
   const srcDir = "src"
   const dstDir = "public"
 
   config.addPassthroughCopy(`${srcDir}/files`)
   config.addPassthroughCopy(`${srcDir}/**/*.css`)
   // config.addPassthroughCopy(`${srcDir}/**/*.js`)
+
+  //config.addPassthroughCopy(`${srcDir}/_collections/projects/**/*`)
 
   return {
     dir: {
---
+++ src/_collections/projects/snow25.md
@@ -0,0 +1,8 @@
+---
+id: "snow25"
+title: "snow25"
+tags: "project"
+---
+
+[click here](/files/snow25/) to to see the project!
+
