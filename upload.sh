04ec989960c89c38c523c5f3dbac428c84fac8bc
Fran Rojo <franmorojo@gmail.com>
2025-07-26 14:03:17 -0400

    autocommit :)

---
+++ upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# stop and quit if a command returns and error
+set -e
+
+# build
+npm run build
+
+# auto git commit and upload
+git add .
+git commit -m "autocommit :)"
+git push
+
+# upload to droplet
+rsync -rP -e ssh public/ root@165.227.199.25:/srv/www/sandbox/
+
+# cp -r public/* /srv/www/sandbox/
