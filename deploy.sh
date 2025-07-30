f5e8e0e03d65a9365fdce3cd35d908821004436a
Fran Rojo <franmorojo@gmail.com>
2025-07-30 18:28:52 -0400

    autocommit :)

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,3 @@ git push
 # upload to droplet
 rsync -rP -e ssh public/ root@165.227.199.25:/srv/www/sandbox/
 
-
-# todo
-# - switch over to srcht
-# - ssh key srcht?
-# - autoamte push from srcht? but then how would i handle the assets...
