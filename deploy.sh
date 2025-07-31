fd3a9e8c79c062abce3fc08c6a9ce75c768917db
Fran Rojo <franmorojo@gmail.com>
2025-07-31 19:24:04 -0400

    autocommit :)

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,15 @@ npm run build
 # auto git commit and upload
 git add .
 git status
-echo "done status"
+
+# this will give non-zero exit code when nothing to commit
 git commit -m "autocommit :)"
 
-echo "done comiting"
 git push
 
-echo "done pushing"
 
 # upload to droplet
 rsync -rP -e ssh public/ root@165.227.199.25:/srv/www/sandbox/
+
+echo "deployed!!"
 
